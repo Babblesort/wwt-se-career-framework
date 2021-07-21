8ee20a3a311c1f1c24b777df1cf0452afbf56f37
Paul McConnell <paul.mcconnell@wwt.com>
2021-07-20 22:40:59 -0500

    create se grid layout - pm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="se-grid">
+      <div />
+      <div className="job-title">Software Engineer Apprentice</div>
+      <div className="job-title">Software Engineer 1</div>
+      <div className="job-title">Software Engineer 2</div>
+      <div className="job-title">Software Engineer 3</div>
+      <div className="job-title">Software Engineer 4</div>
+      <div className="job-title">Software Engineer 5</div>
+      <div className="job-title">Lead Software Engineer</div>
+      <div className="job-title">Software Architect</div>
+
+      <div className="skill">Agile Engineering Best Practices</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+
+      <div className="skill">Application Development & Programming</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency applying">Applying</div>
+
+      <div className="skill">Architecture Design</div>
+      <div />
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency shaping">Shaping</div>
+
+      <div className="skill">Technical Communication & Presentation</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency shaping">Shaping</div>
+
+      <div className="skill">Technical Leadership</div>
+      <div />
+      <div />
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
+
+      <div className="skill">Technology Advising & Consulting</div>
+      <div />
+      <div />
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency shaping">Shaping</div>
+
+      <div className="skill">Technical Requirements Analysis</div>
+      <div className="proficiency developing">Developing</div>
+      <div className="proficiency supporting">Supporting</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency applying">Applying</div>
+      <div className="proficiency leading">Leading</div>
+      <div className="proficiency leading">Leading</div>
     </div>
   );
 }
