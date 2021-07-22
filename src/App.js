a925a167d813f8c7757f285bd38b581445949e52
Paul McConnell <paul.mcconnell@wwt.com>
2021-07-21 19:47:39 -0500

    track active jobTitle when clicked - pm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { jobTitles, jobSkills } from './job-data';
 import JobTitlesRow from './JobTitlesRow';
 import SkillRow from './SkillRow';
 import './app.css';
 
 const App = () => {
+  const [activeJobTitle, setActiveJobTitle] = useState(null);
+
   return (
     <div className="se-grid">
-      <JobTitlesRow titles={jobTitles} />
+      <JobTitlesRow
+        titles={jobTitles}
+        activeJobTitle={activeJobTitle}
+        onClick={setActiveJobTitle}
+      />
 
       {jobSkills.map((skill) => (
         <SkillRow skill={skill} key={skill} />
--- src/JobTitlesRow.js
+++ src/JobTitlesRow.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
+const JobTitlesRow = ({ titles, activeJobTitle, onClick }) => {
+  const handleClick = (title) => {
+    const nextActiveTitle = title === activeJobTitle ? null : title;
+    onClick(nextActiveTitle);
+  };
 
-const JobTitlesRow = ({ titles }) => {
   return (
     <>
       <div />
-      {titles.map((title) => (
-        <div className="job-title" key={title}>
-          {title}
-        </div>
-      ))}
+      {titles.map((title) => {
+        const titleClass = classNames('job-title', {
+          inactive: activeJobTitle != null && title !== activeJobTitle,
+        });
+
+        return (
+          <button
+            type="button"
+            className={titleClass}
+            key={title}
+            onClick={() => handleClick(title)}
+          >
+            {title}
+          </button>
+        );
+      })}
     </>
   );
 };
